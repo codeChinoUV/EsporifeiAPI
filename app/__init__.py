8c0a77a5ca7e27730b15550672b5bed6d31b9564
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-03 15:14:28 -0500

    Se agrego el framework flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 base_de_datos = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(settings_module="config.dev"):
     app = Flask(__name__)
     app.config.from_object(settings_module)
     base_de_datos.init_app(app)
+    migrate.init_app(app, base_de_datos)
     from app.manejo_de_usuarios import manejo_de_usuarios
     app.register_blueprint(manejo_de_usuarios)
     from app.administracion_de_contenido import administracion_de_contenido
     app.register_blueprint(administracion_de_contenido)
-    base_de_datos.create_all()
     return app
