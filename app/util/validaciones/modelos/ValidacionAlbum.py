ddf714571f12e1b69c9013b3f818545873cc7767
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-03 23:15:54 -0500

    Se crearon las validaciones del Album

---
+++ app/util/validaciones/modelos/ValidacionAlbum.py
@@ -0,0 +1,50 @@
+from app.util.validaciones.ValidacioCadenas import ValidacionCadenas
+
+
+class ValidacionAlbum():
+
+    @staticmethod
+    def _validar_parametros_requeridos(album):
+        parametros_faltantes = ""
+        if album.nombre is None:
+            parametros_faltantes += "<nombre>, "
+        if album.anio_lanzamiento is None:
+            parametros_faltantes += "<anio_lanzamiento>"
+
+        if len(parametros_faltantes) > 0:
+            mensaje = "Los siguientes parametros faltan en tu solicitud: " + parametros_faltantes
+            error = {'error': 'pametros_faltantes',
+                     'mensaje': mensaje}
+            return error
+
+    @staticmethod
+    def _validar_tamano_atributos_texto(album):
+        tamano_minino_nombre = 5
+        tamano_maximo_nombre = 70
+        if album.nombre is not None:
+            error = ValidacionCadenas.validar_tamano_parametro(album.nombre, "nombre", tamano_minino_nombre,
+                                                               tamano_maximo_nombre)
+            return error
+
+    @staticmethod
+    def _valdidar_anio_lanzamiento(anio_lanzamiento):
+        error = {'error': 'anio_lanzamineto_invalido', 'mensaje': 'El <anio_lanzamiento> debe de ser entero '
+                                                                  'positivo de no mas de 4 cifras'}
+        try:
+            if anio_lanzamiento is not None:
+                anio_lanzamiento = int(anio_lanzamiento)
+                if anio_lanzamiento <= 0:
+                    return error
+        except ValueError:
+            return error
+
+    @staticmethod
+    def validar_registro_album(album):
+        lista_de_errores = []
+        error_campos_requeridos = ValidacionAlbum._validar_parametros_requeridos(album)
+        if error_campos_requeridos is not None:
+            lista_de_errores.append(error_campos_requeridos)
+        error_anio_invalido = ValidacionAlbum._valdidar_anio_lanzamiento(album.anio_lanzamiento)
+        if error_anio_invalido is not None:
+            lista_de_errores.append(error_anio_invalido)
+        return lista_de_errores
