4bd7f346bd96373e7bc29615080d7697206a579e
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-03 23:16:49 -0500

    Se agregaron dos pruebas unitarias del controladorAlbum

---
+++ app/test/test_endpoint_album.py
@@ -0,0 +1,16 @@
+from app.manejo_de_usuarios.modelo.enum.enums import TipoUsuario
+from app.manejo_de_usuarios.modelo.modelos import Usuario
+from app.util.validaciones.modelos.ValidacionCreadorDeContenido import ValidacionCreadorDeContenido
+from . import BaseTestClass
+
+
+class AlbumControlador(BaseTestClass):
+
+    def test_validar_creador_de_contenido_no_existe(self):
+        with self.app.app_context():
+            usuario_actual = Usuario(nombre_usuario="creadorDeContenido", contrasena="123456",
+                                     tipo_usuario=TipoUsuario.CreadorDeContenido)
+            error = ValidacionCreadorDeContenido \
+                .validar_creador_de_contenido_existe_a_partir_de_usuario(usuario_actual)
+            codigo_error = "usuario_no_tiene_un_creador_de_contenido"
+            self.assertEqual(codigo_error, error['error'])
