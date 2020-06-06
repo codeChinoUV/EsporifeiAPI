1272dd3bcd1de464c96106cc8703a038185a60df
Unknown <unknown@example.com>
2020-06-05 23:46:58 -0500

    Se creo la BaseTestClass con los metodos setUp y tearDown

--- unit_test/__init__.py
+++ unit_test/__init__.py
@@ -1,6 +1,25 @@
 import unittest
+from app import create_app, base_de_datos
 
 
 class BaseTestClass(unittest.TestCase):
+
     def setUp(self):
-        pass
+        """
+        Inicializa crea una app y un cliente para poder ser usados en las pruebas
+        """
+        self.app = create_app(settings_module="config.testing")
+        self.client = self.app.test_client()
+        # Crea un contexto de aplicación
+        with self.app.app_context():
+            # Crea las tablas de la base de datos
+            base_de_datos.create_all()
+
+    def tearDown(self):
+        """
+        Elimina la base de datos cuando se terminan de ejecutar las pruebas
+        """
+        with self.app.app_context():
+            # Elimina todas las tablas de la base de datos
+            base_de_datos.session.remove()
+            base_de_datos.drop_all()
