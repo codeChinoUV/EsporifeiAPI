1fe8619f2332565fa6738f07d7d3c71884334c23
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-14 02:22:04 -0500

    Se agrego el metodo GET, PATCH y DELETE del endpontListaDeReproduccion

--- unit_test/ListaDeReproduccionTest.py
+++ unit_test/ListaDeReproduccionTest.py
@@ -20,6 +20,22 @@ class ListaDeReproduccionTest(BaseTestClass):
             cantidad_lista_reproduccion = 1
             self.assertEqual(cantidad_lista_reproduccion, len(listas_de_reproduccion_del_usuario))
 
+    def test_editar_lista_de_reproduccion(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(usuario_id=1, nombre="Lista de reproduccion prueba")
+            lista_de_reproduccion.guardar()
+            nuevo_nombre = "Lista de prueba 2"
+            lista_de_reproduccion.editar(nuevo_nombre, None)
+            self.assertEqual(nuevo_nombre, lista_de_reproduccion.nombre)
+
+    def test_eliminar_lista_de_reproduccion(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(usuario_id=1, nombre="Lista de reproduccion prueba")
+            lista_de_reproduccion.guardar()
+            lista_de_reproduccion.eliminar()
+            lista_eliminada = ListaDeReproduccion.obtener_lista_de_reproduccion(1)
+            self.assertEqual(None, lista_eliminada)
+
 
 class ValidacionListaDeReproduccionTest(BaseTestClass):
 
@@ -46,3 +62,41 @@ class ValidacionListaDeReproduccionTest(BaseTestClass):
                 .validar_registro_lista_de_reproduccion(lista_de_reproduccion)
             cantidad_errores = 0
             self.assertEqual(cantidad_errores, len(errores_validacion))
+
+    def test_no_existe_lista_de_reproduccion(self):
+        with self.app.app_context():
+            error_no_existe = ValidacionListaDeReproduccion.validar_no_existe_lista_de_reproduccion(1)
+            codigo_error = "lista_reproduccion_inexistente"
+            self.assertEqual(codigo_error, error_no_existe['error'])
+
+    def test_existe_lista_de_reproduccion(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(nombre="Prueba de lista de reproduccion", usuario_id=1)
+            lista_de_reproduccion.guardar()
+            codigo_de_error = None
+            error_no_existe = ValidacionListaDeReproduccion.validar_no_existe_lista_de_reproduccion(1)
+            self.assertEqual(codigo_de_error, error_no_existe)
+
+    def test_no_es_dueno(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(nombre="Prueba", usuario_id=2)
+            lista_de_reproduccion.guardar()
+            error_no_es_dueno = ValidacionListaDeReproduccion.validar_usuario_es_dueno_de_lista_de_reproduccion(1, 1)
+            codigo_error = "operacion_no_permitida"
+            self.assertEqual(codigo_error, error_no_es_dueno['error'])
+
+    def test_es_dueno(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(nombre="Prueba", usuario_id=1)
+            lista_de_reproduccion.guardar()
+            error_no_es_dueno = ValidacionListaDeReproduccion.validar_usuario_es_dueno_de_lista_de_reproduccion(1, 1)
+            codigo_error = None
+            self.assertEqual(codigo_error, error_no_es_dueno)
+
+    def test_solicitud_vacia(self):
+        with self.app.app_context():
+            lista_de_reproduccion = ListaDeReproduccion(nombre=None, descripcion=None)
+            error_sin_parametros = ValidacionListaDeReproduccion.\
+                validar_edicion_lista_de_reproduccion(lista_de_reproduccion)
+            codigo_error = "solicitud_sin_parametros_a_modificar"
+            self.assertEqual(codigo_error, error_sin_parametros['error'])
