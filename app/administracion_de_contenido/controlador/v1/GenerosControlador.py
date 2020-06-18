9bdf9e516de4febee782b2ea59123f7e82468f0f
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-18 02:04:54 -0500

    se creo el metodo GET del endpoint generosCreadoresDeContenido

--- app/administracion_de_contenido/controlador/v1/GenerosControlador.py
+++ app/administracion_de_contenido/controlador/v1/GenerosControlador.py
@@ -47,3 +47,33 @@ class GeneroCancionControlador(Resource):
         for cancion in canciones:
             canciones_dicionario.append(cancion.obtener_json_con_album())
         return canciones_dicionario, 200
+
+
+class GeneroCreadorDeContenido(Resource):
+
+    def get(self, id_genero):
+        """
+        Se encarga de procesar una solicitud de tipo GET al devolver los creadores de contenido del genero, ordenda
+        por nombre
+        :param id_genero: El id del genero a recuperar
+        :return: Una lista de diccionario y un codigo estado
+        """
+        error_no_existe_genero = ValidacionGenero.validar_existe_genero(id_genero)
+        if error_no_existe_genero is not None:
+            return error_no_existe_genero, 404
+        cantidad = request.args.get('cantidad')
+        pagina = request.args.get('pagina')
+        try:
+            if cantidad is not None and pagina is not None:
+                cantidad = int(cantidad)
+                pagina = int(pagina)
+            else:
+                cantidad = 10
+                pagina = 1
+            creadores_de_contenido = Genero.obtener_cradores_de_contenido_por_genero(id_genero, cantidad, pagina)
+        except ValueError:
+            creadores_de_contenido = Genero.obtener_cradores_de_contenido_por_genero(id_genero)
+        creadores_de_contenido_lista = []
+        for creador_de_contenido in creadores_de_contenido:
+            creadores_de_contenido_lista.append(creador_de_contenido.obtener_json())
+        return creadores_de_contenido_lista, 200
