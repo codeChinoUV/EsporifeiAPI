3d9fb1ea95be307d3b96558569385884e3c671c6
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-25 00:20:07 -0500

    Se crearon los metodos crear_ruta_cancion_personal y guardar_cancion_personal

--- Servidor/app/manejo_de_archivos/controlador/ManejadorDeArchivos.py
+++ Servidor/app/manejo_de_archivos/controlador/ManejadorDeArchivos.py
@@ -27,10 +27,32 @@ class ManejadorDeArchivos:
         return carpeta
 
     @staticmethod
+    def crear_ruta_cancion_personal(id_cancion, calidad):
+        carpeta = "/canciones_personales/" + str(id_cancion) + "/"
+        if calidad == Calidad.ALTA:
+            carpeta += "alta"
+        elif calidad == Calidad.MEDIA:
+            carpeta += "media"
+        elif calidad == Calidad.BAJA:
+            carpeta += "baja"
+        elif calidad == Calidad.ORIGINAL:
+            carpeta += "original"
+        os.makedirs(carpeta, exist_ok=True)
+        return carpeta
+
+    @staticmethod
     def _escribir_archivo(ruta_archivo, archivo):
         try:
             with open(ruta_archivo, 'wb') as f:
                 f.write(archivo)
                 f.close()
         except Exception as ex:
-            # Mostrar log de error
+            print(ex)
+        # Mostrar log de error
+
+    @staticmethod
+    def guardar_cancion_personal(id_cancion, formato, calidad, cancion):
+        ruta = ManejadorDeArchivos.crear_ruta_cancion_personal(id_cancion, calidad)
+        ruta_cancion = ruta + "/" + str(id_cancion) + "." + formato.value
+        ManejadorDeArchivos._escribir_archivo(ruta_cancion, cancion)
+        return ruta_cancion
