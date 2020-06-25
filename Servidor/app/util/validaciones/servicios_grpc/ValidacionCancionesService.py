68b9b43a8cbb39c7cd5268ff87df5967f3f4a40f
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-25 00:22:02 -0500

    Se crearon las validaciones para agregar cancion y cancion personal y para obtener cancion y cancion personal

--- Servidor/app/util/validaciones/servicios_grpc/ValidacionCancionesService.py
+++ Servidor/app/util/validaciones/servicios_grpc/ValidacionCancionesService.py
@@ -1,5 +1,4 @@
 import threading
-from concurrent.futures import thread
 
 from app.util.validaciones.modelos.ValidacionCancion import ValidacionCancion
 from app.util.validaciones.modelos.ValidacionCancionPersonal import ValidacionCancionPersonal
@@ -9,7 +8,7 @@ from app.administracion_de_contenido.modelo.modelos import CreadorDeContenido
 
 from app.util.validaciones.servicios_grpc.ValidacionServiciosGrpc import ValidacionServiciosGrpc
 
-from app.manejo_de_archivos.controlador.AdministradorDeArchivos import AdministradorDeArchivos
+from app.manejo_de_archivos.controlador.ManejadorCanciones import ManejadorCanciones
 
 from Servidor.app.manejo_de_archivos.controlador.ManejadorCanciones import ManejadorCanciones
 
@@ -18,6 +17,11 @@ class ValidacionCancionesService:
 
     @staticmethod
     def _validar_existe_cancion(id_cancion):
+        """
+        Se encarga de validar si existe en la base datos la cancion con el id_cancion
+        :param id_cancion: El id de la cancion a validar si existe
+        :return: None si la cancion existe o un ManejadorDeArchivos_pb2.ErrorGeneral si no existe
+        """
         existe_cancion = ValidacionCancion.validar_existe_cancion(id_cancion)
         if existe_cancion is not None:
             error = ManejadorDeArchivos_pb2.ErrorGeneral()
@@ -27,6 +31,11 @@ class ValidacionCancionesService:
 
     @staticmethod
     def _validar_existe_cancion_personal(id_cancion):
+        """
+        Se encarga de validar si existe la cancion personal con el id indicado
+        :param id_cancion: El id de la cancion personal a validar si existe
+        :return: None si la cancion personal existe o un ManejadorDeArchivos_pb2.ErrorGeneral si no existe
+        """
         existe_cancion = ValidacionCancionPersonal.validar_existe_cancion_personal(id_cancion)
         if existe_cancion is not None:
             error = ManejadorDeArchivos_pb2.ErrorGeneral()
@@ -36,6 +45,13 @@ class ValidacionCancionesService:
 
     @staticmethod
     def _validar_es_dueno_de_cancion(token, id_cancion):
+        """
+        Se encarga de validar si la cancion con el id_cancion le pertenece al usuario logeado
+        :param id_cancion: El id de la cancion a validar si el usuario es dueño
+        :param token: El token del usuario logeado
+        :return: None si el usuario si es dueño de la cancion o un ManejadorDeArchivos_pb2.ErrorGeneral si el usuario
+         no es el dueño de la cancion
+        """
         usuario_actual = LoginControlador.token_requerido_grpc(token)
         creador_de_contenido = CreadorDeContenido.obtener_creador_de_contenido_por_id_usuario(usuario_actual.id_usuario)
         es_dueno = ValidacionCancion.\
@@ -48,6 +64,13 @@ class ValidacionCancionesService:
 
     @staticmethod
     def _validar_es_dueno_de_cancion_personal(token, id_cancion):
+        """
+        Se encarga de validar si la cancion personal con el id_cancion le pertenece al usuario logeado
+        :param id_cancion: El id de la cancion perosnal a validar si el usuario es dueño
+        :param token: El token del usuario logeado
+        :return: None si el usuario si es dueño de la cancion o un ManejadorDeArchivos_pb2.ErrorGeneral si el usuario
+         no es el dueño de la cancion
+        """
         usuario_actual = LoginControlador.token_requerido_grpc(token)
         es_dueno = ValidacionCancionPersonal.validar_es_dueno_cancion_personal(usuario_actual.id_usuario, id_cancion)
         if es_dueno is not None:
@@ -57,26 +80,81 @@ class ValidacionCancionesService:
             return error
 
     @staticmethod
-    def _validar_existe_cancion(id_cancion, calidad):
-        existe_cancion = AdministradorDeArchivos.validar_existe_cancion_original(id_cancion)
+    def _validar_existe_cancion_en_servidor_archivos(id_cancion, calidad):
+        """
+        Valida si existe el registro del archivo de la cancion en la base de datos con la calidad indicada
+        :param id_cancion: El id de la cancion a validar si existe
+        :param calidad: La calidad de la cancion a validar si existe
+        :return: None si existe el registro del archivo de la cancion o un ManejadorDeArchivos_pb2.ErrorGeneral si no
+        existe
+        """
+        existe_cancion = ManejadorCanciones.validar_existe_cancion_original(id_cancion)
         if not existe_cancion:
             error_no_existe_cancion = ManejadorDeArchivos_pb2.ErrorGeneral()
             error_no_existe_cancion.error = "cancion_no_existe"
             error_no_existe_cancion.mensaje = "La cancion no se encuentra registrada"
             return error_no_existe_cancion
-        existe_archivo_cancion = AdministradorDeArchivos.validar_existe_archivo_cancion_original(id_cancion)
+        existe_archivo_cancion = ManejadorCanciones.validar_existe_archivo_cancion_original(id_cancion)
         if not existe_archivo_cancion:
             error_no_existe_cancion = ManejadorDeArchivos_pb2.ErrorGeneral()
             error_no_existe_cancion.error = "cancion_no_existe"
             error_no_existe_cancion.mensaje = "La cancion se encuentra registrada, pero no existe el archivo"
             return error_no_existe_cancion
-        existe_cancion_calidad = AdministradorDeArchivos.validar_existe_cancion(id_cancion, calidad)
-        if not existe_cancion_calidad:
+        existe_cancion_calidad = ManejadorCanciones.validar_existe_cancion(id_cancion, calidad)
+        existe_archivo_cancion_calidad = ManejadorCanciones.validar_existe_archivo_cancion(id_cancion, calidad)
+        if not existe_cancion_calidad or not existe_archivo_cancion_calidad:
             hilo_reconvertidor = threading.Thread(target=ManejadorCanciones.reconvertir_cancion, args=id_cancion)
-            # Falta colocar el error y retornarlo
+            hilo_reconvertidor.start()
+            error_no_disponible = ManejadorDeArchivos_pb2.ErrorGeneral()
+            error_no_disponible.error = "cancion_no_disponible"
+            error_no_disponible.mensaje = "La cancion no se encuentra disponible por el momento, " \
+                                          "porfavor espere unos minutos y reintentelo de nuevo"
+            return error_no_disponible
+
+    @staticmethod
+    def _validar_existe_cancion_personal_en_servidor_archivos(id_cancion, calidad):
+        """
+        Valida si existe el registro del archivo de la cancion en la base de datos con la calidad indicada
+        :param id_cancion: El id de la cancion a validar si existe
+        :param calidad: La calidad de la cancion a validar si existe
+        :return: None si existe el registro del archivo de la cancion o un ManejadorDeArchivos_pb2.ErrorGeneral si no
+        existe
+        """
+        existe_cancion_personal = ManejadorCanciones.validar_existe_cancion_personal_original(id_cancion)
+        if not existe_cancion_personal:
+            error_no_existe_cancion = ManejadorDeArchivos_pb2.ErrorGeneral()
+            error_no_existe_cancion.error = "cancion_personal_no_existe"
+            error_no_existe_cancion.mensaje = "La cancion personal no se encuentra registrada"
+            return error_no_existe_cancion
+        existe_archivo_cancion_personal = \
+            ManejadorCanciones.validar_existe_archivo_cancion_personal_original(id_cancion)
+        if not existe_archivo_cancion_personal:
+            error_no_existe_cancion = ManejadorDeArchivos_pb2.ErrorGeneral()
+            error_no_existe_cancion.error = "cancion_personal_no_existe"
+            error_no_existe_cancion.mensaje = "La cancion personal se encuentra registrada, pero no existe el archivo"
+            return error_no_existe_cancion
+        existe_cancion_personal_calidad = ManejadorCanciones.validar_existe_cancion_personal(id_cancion, calidad)
+        existe_archivo_cancion_personal_calidad = ManejadorCanciones.validar_existe_archivo_cancion_personal(id_cancion,
+                                                                                                             calidad)
+        if not existe_cancion_personal_calidad or not existe_archivo_cancion_personal_calidad:
+            hilo_reconvertidor = threading.Thread(target=ManejadorCanciones.reconvertir_cancion_personal,
+                                                  args=id_cancion)
+            hilo_reconvertidor.start()
+            error_no_disponible = ManejadorDeArchivos_pb2.ErrorGeneral()
+            error_no_disponible.error = "cancion_personal_no_disponible"
+            error_no_disponible.mensaje = "La cancion personal no se encuentra disponible por el momento, " \
+                                          "porfavor espere unos minutos y reintentelo de nuevo"
+            return error_no_disponible
 
     @staticmethod
     def validar_agregar_cancion(token, id_cancion):
+        """
+        Se encarga de realizar las validaciones necesarias sobre la cancion para registrar una nueva cancion
+        :param token: El token del usuario logeado
+        :param id_cancion: El id de la cancion que se registrara
+        :return: None si no ocurrio ningun error al momento de realizar las validaciones o
+        un ManejadorDeArchivos_pb2.ErrorGeneral si ocurrio un error
+        """
         error_token_vacio = ValidacionServiciosGrpc.validar_token_vacio(token)
         if error_token_vacio is not None:
             return error_token_vacio
@@ -95,6 +173,12 @@ class ValidacionCancionesService:
 
     @staticmethod
     def validar_agregar_cancion_personal(token, id_cancion):
+        """
+        Se encarga de realizar las validaciones correspondientes al momento de agregar una cancion personal
+        :param token: El token del usuario logeado
+        :param id_cancion: El id de la cancion personal a agregar
+        :return: None si la cancion personal cumple con lo necesario o un ManejadorDeArchivos_pb2.ErrorGeneral si no
+        """
         error_token_vacio = ValidacionServiciosGrpc.validar_token_vacio(token)
         if error_token_vacio is not None:
             return error_token_vacio
@@ -109,7 +193,15 @@ class ValidacionCancionesService:
             return error_no_es_dueno
 
     @staticmethod
-    def validar_obtener_cancion(token, id_cancion):
+    def validar_obtener_cancion(token, id_cancion, calidad):
+        """
+        Realiza las validaciones necesarias para obtener una cancion
+        :param token: El token del usuario logeado
+        :param id_cancion: El id de la cancion a obtener
+        :param calidad: La calidad de la cancion a recuperar
+        :return: None si la cancion con en la calidad indicada es valida o un ManejadorDeArchivos_pb2.ErrorGeneral si
+         ocurrio un error en la validacion
+        """
         error_token_vacio = ValidacionServiciosGrpc.validar_token_vacio(token)
         if error_token_vacio is not None:
             return error_token_vacio
@@ -119,9 +211,21 @@ class ValidacionCancionesService:
         error_no_existe_cancion = ValidacionCancionesService._validar_existe_cancion(id_cancion)
         if error_no_existe_cancion is not None:
             return error_no_existe_cancion
+        error_no_existe_cancion_servidor_archivos = \
+            ValidacionCancionesService._validar_existe_cancion_en_servidor_archivos(id_cancion, calidad)
+        if error_no_existe_cancion_servidor_archivos is not None:
+            return error_no_existe_cancion_servidor_archivos
 
     @staticmethod
-    def validar_obtener_cancion_personal(token, id_cancion):
+    def validar_obtener_cancion_personal(token, id_cancion, calidad):
+        """
+        Realiza las validaciones necesarias para obtener una cancion personal
+        :param token: El token del usuario logeado
+        :param id_cancion: El id de la cancion personal a obtener
+        :param calidad: La calidad de la cancion pesonal a recuperar
+        :return: None si la cancion personal con en la calidad indicada es valida o un
+        ManejadorDeArchivos_pb2.ErrorGeneral si ocurrio un error en la validacion
+        """
         error_token_vacio = ValidacionServiciosGrpc.validar_token_vacio(token)
         if error_token_vacio is not None:
             return error_token_vacio
@@ -131,4 +235,7 @@ class ValidacionCancionesService:
         error_no_existe_cancion = ValidacionCancionesService._validar_existe_cancion_personal(id_cancion)
         if error_no_existe_cancion is not None:
             return error_no_existe_cancion
-        # Falta validar si la cancion con la calidad indicada existe en los registros
+        error_no_existe_cancion_servidor_archivos = \
+            ValidacionCancionesService._validar_existe_cancion_personal_en_servidor_archivos(id_cancion, calidad)
+        if error_no_existe_cancion_servidor_archivos is not None:
+            return error_no_existe_cancion_servidor_archivos
