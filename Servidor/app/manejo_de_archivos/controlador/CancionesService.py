2a4ad389b01b00109e54ad866c6faf0a582270a5
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-06-29 00:25:51 -0500

    Se creo el metodo subir cancion y subir cancion personal

--- Servidor/app/manejo_de_archivos/controlador/CancionesService.py
+++ Servidor/app/manejo_de_archivos/controlador/CancionesService.py
@@ -12,7 +12,28 @@ class CancionesServicer(ManejadorDeArchivos_pb2_grpc.CancionesServicer):
     def SubirCancionPersonal(self, request_iterator, context):
         cancion = bytearray()
         sha256 = ""
+        formato = None
+        id_cancion_personal = 0
+        ya_se_valido = False
+        respuesta = ManejadorDeArchivos_pb2.RespuestaSolicitudSubirArchivo()
+        for request in request_iterator:
+            cancion += bytearray(request.data)
+            sha256 = request.informacionCancion.sha256
+            formato = request.informacionCancion.formatoCancion
+            id_cancion_personal = request.informacionCancion.idCancion
+            if not ya_se_valido:
+                error_validacion = \
+                    ValidacionCancionesService.validar_agregar_cancion_personal(request.token_autenticacion,
+                                                                                request.informacionCancion.idCancion)
+                ya_se_valido = True
+                if error_validacion is not None:
+                    respuesta.error = error_validacion.error
+                    return respuesta
+        ManejadorCanciones.guardar_cancion_personal(cancion, id_cancion_personal, formato, sha256)
 
+    def SubirCancion(self, request_iterator, context):
+        cancion = bytearray()
+        sha256 = ""
         formato = None
         id_cancion = 0
         ya_se_valido = False
@@ -29,12 +50,8 @@ class CancionesServicer(ManejadorDeArchivos_pb2_grpc.CancionesServicer):
                 ya_se_valido = True
                 if error_validacion is not None:
                     respuesta.error = error_validacion.error
-                    respuesta.mensaje = error_validacion.mensaje
                     return respuesta
         ManejadorCanciones.guardar_cancion(cancion, id_cancion, formato, sha256)
-
-    def SubirCancion(self, request_iterator, context):
-        pass
 
     def ObtenerCancion(self, request, context):
         pass
