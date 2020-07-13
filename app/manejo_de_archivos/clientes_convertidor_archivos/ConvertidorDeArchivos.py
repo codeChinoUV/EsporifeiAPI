9abfd0dfd5d1c556dc5019ba9add90332a02ac1c
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-13 04:04:43 -0500

    Se documento la clase ConvertidorDeArchivos

--- app/manejo_de_archivos/clientes_convertidor_archivos/ConvertidorDeArchivos.py
+++ app/manejo_de_archivos/clientes_convertidor_archivos/ConvertidorDeArchivos.py
@@ -28,6 +28,11 @@ class ConvertidorDeArchivos:
         return ConvertidorDeArchivos.__convertidor_de_archivos
 
     def agregar_portada_album_a_cola(self, id_album):
+        """
+        Agrega una portada a la cola de convertir archivos
+        :param id_album: El id del album a agregar a la cola
+        :return: None
+        """
         if id_album not in ConvertidorDeArchivos._cola_convertidor_portadas_album:
             ConvertidorDeArchivos._cola_convertidor_portadas_album.append(id_album)
             if ConvertidorDeArchivos._cantidad_hilos_ejecutandose < 5 and \
@@ -35,6 +40,11 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_portada_album()
 
     def agregar_portada_creador_de_contenido_a_cola(self, id_creador_de_contenido):
+        """
+        Agrega una portada a la cola de convertir archivos
+        :param id_creador_de_contenido: El id del creador de contenido a agregar a la cola
+        :return: None
+        """
         if id_creador_de_contenido not in ConvertidorDeArchivos._cola_convertidor_portadas_creador_de_contendio:
             ConvertidorDeArchivos._cola_convertidor_portadas_creador_de_contendio.append(id_creador_de_contenido)
             if ConvertidorDeArchivos._cantidad_hilos_ejecutandose < 5 and \
@@ -42,6 +52,11 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_portada_creador_de_contenido()
 
     def agregar_porada_usuario_a_cola(self, id_usuario):
+        """
+        Agrega una portada a la cola de convertir archivos
+        :param id_usuario: El id del usuario a agregar a la cola
+        :return: None
+        """
         if id_usuario not in ConvertidorDeArchivos._cola_convertidor_portadas_usuario:
             ConvertidorDeArchivos._cola_convertidor_portadas_usuario.append(id_usuario)
             if ConvertidorDeArchivos._cantidad_hilos_ejecutandose < 5 and \
@@ -49,6 +64,11 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_portada_usuario()
 
     def agregar_cancion_a_cola(self, id_cancion):
+        """
+        Agrega una cancion a la cola de convertir archivos
+        :param id_cancion: El id de la cancion a agregar a la cola
+        :return: None
+        """
         if id_cancion not in ConvertidorDeArchivos._cola_convertidor_canciones:
             ConvertidorDeArchivos._cola_convertidor_canciones.append(id_cancion)
             if ConvertidorDeArchivos._cantidad_hilos_ejecutandose < 5 and \
@@ -56,6 +76,11 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_cancion()
 
     def agregar_cancion_personal_a_cola(self, id_cancion_personal):
+        """
+        Agrega una cancionPersonal a la cola de convertir archivos
+        :param id_cancion_personal: El id de la cancion personal a agregar a la cola
+        :return: None
+        """
         if id_cancion_personal not in ConvertidorDeArchivos._cola_convertidor_canciones_personales:
             ConvertidorDeArchivos._cola_convertidor_canciones_personales.append(id_cancion_personal)
             if ConvertidorDeArchivos._cantidad_hilos_ejecutandose < 5 and \
@@ -63,26 +88,54 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_cancion_personal()
 
     def _convertir_cancion(self, id_cancion):
+        """
+        Llama al metodo de convertir cancion y la quita de la cola cuando el metodo se termina de convertir
+        :param id_cancion: El id de la cancion a convertir
+        :return: None
+        """
         ManejadorCanciones.convertir_cancion_mp3_todas_calidades(id_cancion)
         self._quitar_de_cola_canciones()
 
     def _convertir_cancion_personal(self, id_cancion_personal):
+        """
+        Llama al metodo de convertir cancion_personal y la quita de la cola cuando el metodo se termina de convertir
+        :param id_cancion_personal: El id de la cancion personal a convertir
+        :return: None
+        """
         ManejadorCanciones.convertir_cancion_personal_mp3_todas_calidades(id_cancion_personal)
         self._quitar_de_cola_canciones_personales()
 
     def _convertir_portada_album(self, id_album):
+        """
+        Llama al metodo de convertir portada y la quita de la cola cuando el metodo se termina de convertir
+        :param id_album: El id del album a convertir
+        :return: None
+        """
         ManejadorDePortadas.convertir_portada_todas_calidades(id_album, TipoPortada.ALBUM)
         self._quitar_de_cola_portada_album()
 
     def _convertir_portada_creador_de_contenido(self, id_creador_de_contenido):
+        """
+        Llama al metodo de convertir convertir portada y la quita de la cola cuando el metodo se termina de convertir
+        :param id_creador_de_contenido: El id del creador de contenido a convertir
+        :return: None
+        """
         ManejadorDePortadas.convertir_portada_todas_calidades(id_creador_de_contenido, TipoPortada.CREADOR_DE_CONTENIDO)
         self._quitar_de_cola_portada_creadores_de_contenido()
 
     def _convertir_portada_usuario(self, id_usuario):
+        """
+        Llama al metodo de convertir portada y la quita de la cola cuando el metodo se termina de convertir
+        :param id_usuario: El id del usuario a convertir
+        :return: None
+        """
         ManejadorDePortadas.convertir_portada_todas_calidades(id_usuario, TipoPortada.USUARIO)
         self._quitar_de_cola_portada_usuario()
 
     def _quitar_de_cola_canciones(self):
+        """
+        Quita la primera cancion de la cola
+        """
         with ConvertidorDeArchivos._lock:
             ConvertidorDeArchivos._cola_convertidor_canciones.pop()
             ConvertidorDeArchivos._cantidad_hilos_ejecutandose -= 1
@@ -92,12 +145,18 @@ class ConvertidorDeArchivos:
             self._crear_hilo_convertir_cancion()
 
     def _crear_hilo_convertir_cancion(self):
+        """
+        Crea un hilo para convertir una cancion
+        """
         ConvertidorDeArchivos._executor.submit(self._convertir_cancion,
                                                ConvertidorDeArchivos._cola_convertidor_canciones[0])
         ConvertidorDeArchivos._cancion_actual_convirtiendo = ConvertidorDeArchivos._cola_convertidor_canciones[0]
         ConvertidorDeArchivos._cantidad_hilos_ejecutandose += 1
 
     def _crear_hilo_convertir_cancion_personal(self):
+        """
+        Crea un hilo para convertir una cancion personal
+        """
         ConvertidorDeArchivos._executor.submit(self._convertir_cancion_personal,
                                                ConvertidorDeArchivos._cola_convertidor_canciones_personales[0])
         ConvertidorDeArchivos._cancion_personal_actual_convirtiendo = \
@@ -105,6 +164,9 @@ class ConvertidorDeArchivos:
         ConvertidorDeArchivos._cantidad_hilos_ejecutandose += 1
 
     def _quitar_de_cola_canciones_personales(self):
+        """
+        Quita la primera cancion personal de la cola
+        """
         with ConvertidorDeArchivos._lock:
             ConvertidorDeArchivos._cola_convertidor_canciones_personales.pop()
             ConvertidorDeArchivos._cantidad_hilos_ejecutandose -= 1
@@ -114,6 +176,9 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_cancion_personal()
 
     def _crear_hilo_convertir_portada_album(self):
+        """
+        Crea un hilo para convertir una portada album
+        """
         ConvertidorDeArchivos._executor.submit(self._convertir_portada_album,
                                                ConvertidorDeArchivos._cola_convertidor_portadas_album[0])
         ConvertidorDeArchivos._portada_album_actual_convirtiendo = \
@@ -121,6 +186,9 @@ class ConvertidorDeArchivos:
         ConvertidorDeArchivos._cantidad_hilos_ejecutandose += 1
 
     def _quitar_de_cola_portada_album(self):
+        """
+        Quita la primera portada album de la cola
+        """
         with ConvertidorDeArchivos._lock:
             ConvertidorDeArchivos._cola_convertidor_portadas_album.pop()
             ConvertidorDeArchivos._cantidad_hilos_ejecutandose -= 1
@@ -130,6 +198,9 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_cancion_personal()
 
     def _crear_hilo_convertir_portada_creador_de_contenido(self):
+        """
+        Crea un hilo para convertir una cancion personal
+        """
         ConvertidorDeArchivos._executor.submit(self._convertir_portada_creador_de_contenido,
                                                ConvertidorDeArchivos._cola_convertidor_portadas_creador_de_contendio[0])
         ConvertidorDeArchivos._portada_creador_de_contenido_actual_convirtiendo = \
@@ -137,6 +208,9 @@ class ConvertidorDeArchivos:
         ConvertidorDeArchivos._cantidad_hilos_ejecutandose += 1
 
     def _quitar_de_cola_portada_creadores_de_contenido(self):
+        """
+        Quita de la cola creador de contenido
+        """
         with ConvertidorDeArchivos._lock:
             ConvertidorDeArchivos._cola_convertidor_portadas_creador_de_contendio.pop()
             ConvertidorDeArchivos._cantidad_hilos_ejecutandose -= 1
@@ -146,6 +220,9 @@ class ConvertidorDeArchivos:
                 self._crear_hilo_convertir_portada_creador_de_contenido()
 
     def _crear_hilo_convertir_portada_usuario(self):
+        """
+        Crea un hilo para convertir una portada usuario
+        """
         ConvertidorDeArchivos._executor.submit(self._convertir_portada_usuario,
                                                ConvertidorDeArchivos._cola_convertidor_portadas_usuario[0])
         ConvertidorDeArchivos._portada_usuario_actual_convirtiendo = \
@@ -153,6 +230,9 @@ class ConvertidorDeArchivos:
         ConvertidorDeArchivos._cantidad_hilos_ejecutandose += 1
 
     def _quitar_de_cola_portada_usuario(self):
+        """
+        Quita de la cola la primera portada de la cola usuario
+        """
         with ConvertidorDeArchivos._lock:
             ConvertidorDeArchivos._cola_convertidor_portadas_usuario.pop()
             ConvertidorDeArchivos._cantidad_hilos_ejecutandose -= 1
