095d75ad38af71882dd08231a588b3b64fb870c9
José Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-16 14:44:10 -0500

    Se creo el docker-compose

--- config/prod.py
+++ config/prod.py
@@ -2,4 +2,4 @@ from .default import *
 
 SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
 APP_ENV = APP_ENV_PRODUCTION
-SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
+SQLALCHEMY_DATABASE_URI = "postgresql://espotifeiapi:123456@psql_db:5432/espotifei"
