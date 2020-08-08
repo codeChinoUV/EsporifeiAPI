8ba4cbc7aa77147ce384d084d6740eed3f0c93b3
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-08-08 02:24:38 -0500

    se arregla bug de yield

--- convertidor_archivos/ConvertidorDeArchivosServer.py
+++ convertidor_archivos/ConvertidorDeArchivosServer.py
@@ -35,58 +35,59 @@ class ConvertidorDeCancionesServicer(ConvertidorDeArchivos_pb2_grpc.ConvertidorD
                 logger.error("integridad_archivo_no_definida_" + str(convertidor_de_canciones.id_cancion))
                 respuesta.error.error = "integridad_archivo_no_definida"
                 respuesta.error.mensaje = "El Hash 256 del archivo recibido no coincide con el del archivo enviado"
-                return respuesta
-            cancion_calidad_alta = convertidor_de_canciones.convertir_a_wav_calidad_alta()
-            cancion_calidad_media = convertidor_de_canciones.convertir_a_wav_calidad_media()
-            cancion_calidad_baja = convertidor_de_canciones.convertir_a_mp3_calidad_baja()
-            bytes_del_chunk_baja = leer_archivo_por_bloques(cancion_calidad_baja,
-                                                            convertidor_de_canciones.TAMANO_CHUNK)
-            bytes_del_chunk_media = leer_archivo_por_bloques(cancion_calidad_media,
-                                                             convertidor_de_canciones.TAMANO_CHUNK)
-            bytes_del_chunk_alta = leer_archivo_por_bloques(cancion_calidad_alta,
-                                                            convertidor_de_canciones.TAMANO_CHUNK)
-            hash256_cancion_calidad_baja = obtener_has256_de_archivo(cancion_calidad_baja)
-            hash256_cancion_calidad_media = obtener_has256_de_archivo(cancion_calidad_media)
-            hash256_cancion_calidad_alta = obtener_has256_de_archivo(cancion_calidad_alta)
-            log_baja_enviada = False
-            log_media_enviada = False
-            while True:
-                try:
-                    respuesta.cancionCalidadBaja.data = next(bytes_del_chunk_baja)
-                    respuesta.cancionCalidadBaja.informacionArchivo.hash256 = str(hash256_cancion_calidad_baja)
-                    respuesta.cancionCalidadBaja.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
-                except StopIteration:
-                    respuesta.cancionCalidadBaja.data = bytes()
-                    if not log_baja_enviada:
-                        logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
-                                    + convertidor_de_canciones.FORMATO_MP3 + " en calidad baja")
-                        log_baja_enviada = True
-                try:
-                    respuesta.cancionCalidadMedia.data = next(bytes_del_chunk_media)
-                    respuesta.cancionCalidadMedia.informacionArchivo.hash256 = hash256_cancion_calidad_media
-                    respuesta.cancionCalidadMedia.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
-                except StopIteration:
-                    respuesta.cancionCalidadMedia.data = bytes()
-                    if not log_media_enviada:
-                        logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
-                                    + convertidor_de_canciones.FORMATO_MP3 + " en calidad media")
-                        log_media_enviada = True
-                try:
-                    respuesta.cancionCalidadAlta.data = next(bytes_del_chunk_alta)
-                    respuesta.cancionCalidadAlta.informacionArchivo.hash256 = hash256_cancion_calidad_alta
-                    respuesta.cancionCalidadAlta.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
-                except StopIteration:
-                    logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
-                                + convertidor_de_canciones.FORMATO_MP3 + " en calidad alta")
-                    break
                 yield respuesta
-            convertidor_de_canciones.limpiar_archivos()
-            logger.info("Se limpiaron los archivos generados de la cancion con el id "
-                        + str(convertidor_de_canciones.id_cancion))
-        except Exception:
-            convertidor_de_canciones.limpiar_archivos()
-            logger.error("Ocurrio un error y se limpiaron los archivos generados de la cancion con el id "
-                         + str(convertidor_de_canciones.id_cancion))
+            else:
+                cancion_calidad_alta = convertidor_de_canciones.convertir_a_wav_calidad_alta()
+                cancion_calidad_media = convertidor_de_canciones.convertir_a_wav_calidad_media()
+                cancion_calidad_baja = convertidor_de_canciones.convertir_a_mp3_calidad_baja()
+                bytes_del_chunk_baja = leer_archivo_por_bloques(cancion_calidad_baja,
+                                                                convertidor_de_canciones.TAMANO_CHUNK)
+                bytes_del_chunk_media = leer_archivo_por_bloques(cancion_calidad_media,
+                                                                convertidor_de_canciones.TAMANO_CHUNK)
+                bytes_del_chunk_alta = leer_archivo_por_bloques(cancion_calidad_alta,
+                                                                convertidor_de_canciones.TAMANO_CHUNK)
+                hash256_cancion_calidad_baja = obtener_has256_de_archivo(cancion_calidad_baja)
+                hash256_cancion_calidad_media = obtener_has256_de_archivo(cancion_calidad_media)
+                hash256_cancion_calidad_alta = obtener_has256_de_archivo(cancion_calidad_alta)
+                log_baja_enviada = False
+                log_media_enviada = False
+                while True:
+                    try:
+                        respuesta.cancionCalidadBaja.data = next(bytes_del_chunk_baja)
+                        respuesta.cancionCalidadBaja.informacionArchivo.hash256 = str(hash256_cancion_calidad_baja)
+                        respuesta.cancionCalidadBaja.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
+                    except StopIteration:
+                        respuesta.cancionCalidadBaja.data = bytes()
+                        if not log_baja_enviada:
+                            logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
+                                        + convertidor_de_canciones.FORMATO_MP3 + " en calidad baja")
+                            log_baja_enviada = True
+                    try:
+                        respuesta.cancionCalidadMedia.data = next(bytes_del_chunk_media)
+                        respuesta.cancionCalidadMedia.informacionArchivo.hash256 = hash256_cancion_calidad_media
+                        respuesta.cancionCalidadMedia.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
+                    except StopIteration:
+                        respuesta.cancionCalidadMedia.data = bytes()
+                        if not log_media_enviada:
+                            logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
+                                        + convertidor_de_canciones.FORMATO_MP3 + " en calidad media")
+                            log_media_enviada = True
+                    try:
+                        respuesta.cancionCalidadAlta.data = next(bytes_del_chunk_alta)
+                        respuesta.cancionCalidadAlta.informacionArchivo.hash256 = hash256_cancion_calidad_alta
+                        respuesta.cancionCalidadAlta.informacionArchivo.extension = convertidor_de_canciones.FORMATO_MP3
+                    except StopIteration:
+                        logger.info("Se envio la cancion " + str(convertidor_de_canciones.id_cancion) + "."
+                                    + convertidor_de_canciones.FORMATO_MP3 + " en calidad alta")
+                        break
+                    yield respuesta
+                convertidor_de_canciones.limpiar_archivos()
+                logger.info("Se limpiaron los archivos generados de la cancion con el id "
+                            + str(convertidor_de_canciones.id_cancion))
+            except Exception:
+                convertidor_de_canciones.limpiar_archivos()
+                logger.error("Ocurrio un error y se limpiaron los archivos generados de la cancion con el id "
+                            + str(convertidor_de_canciones.id_cancion))
 
 
 class ConvertidorDeImagenesServicer(ConvertidorDeArchivos_pb2_grpc.ConvertidorDeImagenesServicer):
