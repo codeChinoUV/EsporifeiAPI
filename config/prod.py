49bbaaf0bd4d1bec2d81eb35882e5f073b1c149f
codeChinoUV <47763693+codeChinoUV@users.noreply.github.com>
2021-04-10 23:55:00 -0500

    Update prod.py

--- config/prod.py
+++ config/prod.py
@@ -2,4 +2,4 @@ from .default import *
 
 SECRET_KEY = 'putyoursecretkeyhere'
 APP_ENV = APP_ENV_PRODUCTION
-SQLALCHEMY_DATABASE_URI = "postgresql://username:password@host:5432/databse"
+SQLALCHEMY_DATABASE_URI = "postgresql://username:password@psql_db:5432/databse"
